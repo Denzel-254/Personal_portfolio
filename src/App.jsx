e2cbc8693bc2436a6b00042308b6eee8f61ea45e
Denzel-254 <ochiengden335@gmail.com>
2025-11-18 23:29:32 +0300

    Created hero section,header an the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-
-import './App.css'
+import "./App.css";
+import Header from "./components/layout/Header";
+import Hero from "./components/sections/Hero";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  
-
   return (
     <>
-      my app is working
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Hero />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
