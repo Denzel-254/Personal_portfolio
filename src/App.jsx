3f6ccc17646808564c984c227d89cba6f0927417
Denzel-254 <ochiengden335@gmail.com>
2025-11-20 00:10:10 +0300

    Added more pictures

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Hero from "./components/sections/Hero";
 import About from "./components/sections/About";
 import Services from "./components/sections/Services";
 import Footer from "./components/layout/Footer";
+import Techstack from "./components/sections/Techstack";
+import Projects from "./components/sections/Projects";
 import SectionWrapper from "./components/common/Sectionwrapper";
+import Contact from "./components/sections/Contact";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -24,6 +27,18 @@ function App() {
 
                 <SectionWrapper className="bg-gray-900 text-white">
                   <Services />
+                </SectionWrapper>
+
+                <SectionWrapper className="bg-gray-900 text-white">
+                  <Techstack />
+                </SectionWrapper>
+
+                <SectionWrapper className="bg-gray-900 text-white">
+                  <Projects />
+                </SectionWrapper>
+
+                <SectionWrapper className="bg-gray-900 text-white">
+                  <Contact />
                 </SectionWrapper>
 
                 <SectionWrapper className="bg-gray-900 text-white">
